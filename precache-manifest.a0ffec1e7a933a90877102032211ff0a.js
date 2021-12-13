fe1ae2751e525b8f84b2ffa56586edc10e7ac544
Oleksandr Yurchuk <syurchuk.98@gmail.com>
2021-12-13 22:26:16 +0200

    Updates

---
+++ precache-manifest.a0ffec1e7a933a90877102032211ff0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3505f4adc1b9a672802d",
+    "url": "/app-rr/static/css/main.287770f1.chunk.css"
+  },
+  {
+    "revision": "3505f4adc1b9a672802d",
+    "url": "/app-rr/static/js/main.9f733b26.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "1abf9d5679cc246ec9a1",
+    "url": "/app-rr/static/js/2.cd55463e.chunk.js"
+  },
+  {
+    "revision": "6f1fd9b042e6eed2e747a635a27ff0b6",
+    "url": "/app-rr/static/media/img1.6f1fd9b0.jpg"
+  },
+  {
+    "revision": "bb95accc362d61dfad494cecc22d9f3a",
+    "url": "/app-rr/static/media/img3.bb95accc.jpg"
+  },
+  {
+    "revision": "4267dfd49613af2e16c2ed6c03276475",
+    "url": "/app-rr/static/media/img2.4267dfd4.jpg"
+  },
+  {
+    "revision": "ee094ad95210b6d0b7c618d0eb3daa9c",
+    "url": "/app-rr/index.html"
+  }
+];
